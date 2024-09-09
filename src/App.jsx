2010a33a51740db9af6da5ef16b730c3e3f9899b
the3musketeersgh <the3musketeersgh@gmail.com>
2024-09-09 12:46:39 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Shop from "./components/Shop";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Shop />
+    </>
+  );
+}
+
+export default App;
